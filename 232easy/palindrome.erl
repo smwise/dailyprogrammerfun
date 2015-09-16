03d216a627278cab3111c92d00211783b682a293
smwise <smw4429@gmail.com>
2015-09-16 18:40:52 -0500

    yep

--- 232easy/palindrome.erl
+++ 232easy/palindrome.erl
@@ -3,7 +3,7 @@
 -export([check_palindrome/1, check_palindrome/0]).
 
 check_palindrome(Input) when is_list(Input) ->
-    Input0 = re:replace(string:to_upper(Input), "\\s+|\W", "", [global,{return,list}]),
+    Input0 = re:replace(string:to_upper(Input), "\\s+|\\W", "", [global,{return,list}]),
     Input1 = lists:reverse(Input0),
     if Input0 =:= Input1 -> io:format("Yes it's a palindrome\n");
        Input0 =/= Input1 -> io:format("Nope\n")
