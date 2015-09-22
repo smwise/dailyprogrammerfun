859fadb26b23618b2141daebef70947bbb043fba
smwise <smw4429@gmail.com>
2015-09-22 10:21:15 -0500

    added bonus pieces and made it a little less redundant.

--- easy/229/dottie.erl
+++ easy/229/dottie.erl
@@ -1,10 +1,34 @@
 -module(dottie).
--compile(export_all).
+-export([find_dottie/1, find_pi/1, find_golden_ratio/1,
+         find_logistics_map/1]).
 
-find_dottie(X) ->
-    find_dottie(X, 0).
+%% defining the calculations
+dottie_fun(X) -> math:cos(X).
+pi_fun(X) -> X - math:tan(X).
+golden_fun(X) -> 1 + 1/X.
+logistics_fun(X) -> 4*X*(1-X).
 
-find_dottie(X, 100) ->
+%% boiler plate function that gets passed the function
+%% to use to calculate the next value of X
+find_fixed_point(X, _, 100) ->
     X;
-find_dottie(X, Count) ->
-    find_dottie(math:cos(X), Count+1).
+find_fixed_point(X, Manip, Count) ->
+    find_fixed_point(Manip(X), Manip, Count+1).
+
+%% Main problem, cos(x)
+%% Passes in dottie_fun/1
+find_dottie(X) ->
+    find_fixed_point(X, fun dottie_fun/1, 0).
+
+%% Bonus #1, fixed point of x-tan(x)
+%% Uses pi_fun
+find_pi(X) ->
+    find_fixed_point(X, fun pi_fun/1, 0).
+
+%% Bonux #2, fixed point of 1 + 1/x
+find_golden_ratio(X) ->
+    find_fixed_point(X, fun golden_fun/1, 0).
+
+%% Bonus #3, 4X(1-X) don't know what this one is
+find_logistics_map(X) ->
+    find_fixed_point(X, fun logistics_fun/1, 0).
